871a31ed95272c49c815fb32cc0423d8490a98b5
Jesper Brynildsen <jesper_bry@hotmail.com>
2021-06-13 17:06:41 +0200

    Add base header + animation

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles/Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header animated animatedFadeInUp fadeInUp">
+      <h1>Hi, my name is Jesper</h1>
+    </div>
+  );
+};
+
+export default Header;
