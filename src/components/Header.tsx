b1e7b2b78810cb86fb8fb3a8e36405cc08721102
Jesper Brynildsen <jesper_bry@hotmail.com>
2021-06-15 22:44:32 +0200

    Add MediaBar tsx + scss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
+import MediaBar from "./MediaBar";
+
 import "./styles/Header.scss";
 
 const Header = () => {
   return (
     <div className="header animated animatedFadeInUp fadeInUp">
       <h1>Hi, my name is Jesper</h1>
+      <MediaBar />
     </div>
   );
 };
---
+++ src/components/MediaBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles/MediaBar.scss";
+
+const MediaBar = () => {
+  return <div></div>;
+};
+
+export default MediaBar;
