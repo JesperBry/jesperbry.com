d51454aba1e7f1300a370ecc59c4048c861502f0
Jesper Brynildsen <jesper_bry@hotmail.com>
2021-07-03 22:27:17 +0200

    Add base Nav + fix BG scss for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import MediaBar from "./MediaBar";
 import { sosialMedia } from "./utils/MediaArray";
@@ -6,9 +7,15 @@ import { sosialMedia } from "./utils/MediaArray";
 import "./styles/Header.scss";
 
 const Header: React.FC = () => {
+  const history = useHistory();
+
+  const handleClick_H1: VoidFunction = () => {
+    history.push("/");
+  };
+
   return (
     <div className="header animated animatedFadeInUp fadeInUp">
-      <h1>Hi, my name is Jesper</h1>
+      <h1 onClick={() => handleClick_H1()}>Hi, my name is Jesper</h1>
       <MediaBar mediaArr={sosialMedia} />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import "./styles/Navbar.scss";
+
+import BarsIcon from "../assets/icons/grip-lines-solid.svg";
+import CrossIcon from "../assets/icons/times-solid.svg";
+
+const Navbar: React.FC = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const isActive: any = (path: string, match: any, location: any) =>
+    !!(match || path === location.pathname);
+
+  const toggleNav = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <>
+      <div className="navbar">
+        <button className="menuButton" onClick={toggleNav}>
+          {toggle ? (
+            <img src={CrossIcon} alt="" className="menuIcons" />
+          ) : (
+            <img src={BarsIcon} alt="" className="menuIcons" />
+          )}
+        </button>
+        <div className="navLinks">
+          <ul className={toggle ? "links show-nav" : "links"}>
+            <li>
+              <NavLink
+                to={"/about"}
+                activeClassName="nav-link--active"
+                isActive={isActive.bind(this, "/about")}
+              >
+                {"ABOUT"}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/skills"}
+                activeClassName="nav-link--active"
+                isActive={isActive.bind(this, "/skills")}
+              >
+                {"SKILLS"}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/contact"}
+                activeClassName="nav-link--active"
+                isActive={isActive.bind(this, "/contact")}
+              >
+                {"CONTACT"}
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
